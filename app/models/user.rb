efcede00757dd762335401ab9219cddb9ee179c8
saki918 <hiro9181013@gmail.com>
2020-02-06 03:59:46 +0000

    応用課題２終了

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,:validatable
+
+  has_many :books
+  attachment :profile_image, destroy: false
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, length: {minimum: 2,maximum: 20}
+  validates :introduction, length: { maximum: 50 }
+  def current_user?(user)
+    user == current_user
+  end
+end
